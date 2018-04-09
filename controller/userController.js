96950bc750ce1458176d249cbfa401ede41a117e
swatimavani14@gmail.com <swatimavani14@gmail.com>
2018-04-09 08:22:41 +0530

    Change into login api

--- controller/userController.js
+++ controller/userController.js
@@ -7,38 +7,46 @@ function  UserController() {
     this.user = {};
 }
 
+function getExistingUser(deviceId){
+    return User.findOne({deviceId:deviceId,facebookId:null});
+}
 UserController.prototype.login = async function (req,res) {    
-    var userData = _.pick(req.body,['facebookId','deviceId','username','profileLink','data']);
-    var where = {deleted:false};
-
-    if(userData.facebookId){
-        where.facebookId = userData.facebookId;
-        where.deviceId = userData.deviceId;
-
-       var deletedUser =  await User.findOneAndRemove({deviceId:userData.deviceId,facebookId:null});
-       console.log("deleted user " + deletedUser);
-       var deleteTokon = await Token.remove({userId : deletedUser._id});
+    var userData = _.pick(req.body,['facebookId','deviceId','username','profileLink','data','retrieveOldData']);
+    var where = {deleted:false,deviceId:userData.deviceId};   
        
-    }else{
-        where.deviceId = userData.deviceId;       
+    var ExistingUser = await getExistingUser(userData.deviceId);
+    
+    if(ExistingUser && userData.retrieveOldData){
+        var token = await ExistingUser.generateAuthToken(userData.deviceId);       
+        return res.header('x-auth',token).send(ExistingUser);       
+    }else{       
+        where.facebookId = userData.facebookId?userData.facebookId:null;       
+        var dataForUpdate = {username:userData.username};
+        var user = await User.findOneAndUpdate(where,dataForUpdate,{new:true});
+    
+        if(!user){
+            userData.primaryCurrency = config.game.primaryCurrency;
+            userData.secondaryCurrency = config.game.secondaryCurrency;
+            user = await new User(userData).save();
+        }
+        if(user){
+            var token = await user.generateAuthToken(userData.deviceId);       
+            res.header('x-auth',token).send(user);       
+        }else{
+            res.send(user);
+        }
     }
-    var dataForUpdate = {username:userData.username};
-    var user = await User.findOneAndUpdate(where,dataForUpdate,{new:true});
-
-    if(!user){
-        userData.primaryCurrency = config.game.primaryCurrency;
-        userData.secondaryCurrency = config.game.secondaryCurrency;
-        user = await new User(userData).save();
-    }
-    if(user){
-        var token = await user.generateAuthToken(userData.deviceId);       
-        res.header('x-auth',token).send(user);       
-    }else{
-        res.send(user);
-    }
+    
 }
 
 UserController.prototype.details = async function (req,res){   
     res.send(req.user);
 }
 
+async function removeUser(deviceId){
+    var deletedUser = await User.fineOneAndRemove({deviceId:deviceId,facebookId:null});
+    if(deletedUser){
+        await Token.remove({userId:deletedUser._id});
+    }
+    return true;
+}
